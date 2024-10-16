1f8de07ab64bcc57092c84679231260e5a6a4a12
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-16 16:02:41 +0300

    Construct a new route and view

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
 
+///////////////     ROUTES      ///////////////////
+
+
 // GET 
 app.get('/', async (req, res) => {
   res.render('index.ejs')
+})
+
+// GET /fruits/new
+app.get('/fruits/new', (req, res) => {
+    res.render("fruits/new.ejs")
 })
