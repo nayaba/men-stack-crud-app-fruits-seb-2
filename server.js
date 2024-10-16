e6d81170ba3e39d49cfe99c64305291687e2ec4c
Nabila Ayaba <nabilaayaba@gmail.com>
2024-10-16 16:18:58 +0300

    Build create functionality in a POST route

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const morgan = require('morgan')
 
 const app = express()
 
-///////////////    MODELS      ///////////////////
+//////////////////////////////////////////////////////
+///////////////      MODELS    ///////////////////////
+//////////////////////////////////////////////////////
 const Fruit = require('./models/fruit.js')
 
-
-///////////////    MIDDLEWARE      ///////////////////
+//////////////////////////////////////////////////////
+///////////////     MIDDLEWARE     ///////////////////
+//////////////////////////////////////////////////////
+app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 
-
-///////////////     DB CONNECTION    ///////////////////
+//////////////////////////////////////////////////////
+///////////////     DB CONNECTION    /////////////////
+//////////////////////////////////////////////////////
 // Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGODB_URI)
 // log connection status to terminal on start
@@ -22,19 +27,36 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name} 🍒`)
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
-
-///////////////     ROUTES      ///////////////////
-
-
-// GET 
+//////////////////////////////////////////////////////
+///////////////     ROUTES     ///////////////////////
+//////////////////////////////////////////////////////
+// GET
 app.get('/', async (req, res) => {
   res.render('index.ejs')
 })
 
 // GET /fruits/new
 app.get('/fruits/new', (req, res) => {
-    res.render("fruits/new.ejs")
+  res.render('fruits/new.ejs')
+})
+
+// POST /fruits
+app.post('/fruits', async (req, res) => {
+  //   convert this "on" or undefined value to a Boolean
+  const formData = req.body
+  if (req.body.isReadyToEat === 'on') {
+    formData.isReadyToEat = true
+  } else {
+    formData.isReadyToEat = false
+  }
+
+  //   Create fruit in database
+  await Fruit.create(formData)
+
+  //   redirect the user back to the form page
+  res.redirect('/fruits/new')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
 })
